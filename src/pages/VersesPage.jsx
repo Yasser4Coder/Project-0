f536c6f4f84db3ffb8807c795a5635db5135fe8c
Yasser4Coder <by.benseghier.yasser@gmail.com>
2024-03-27 23:04:39 +0100

    d

--- src/pages/VersesPage.jsx
+++ src/pages/VersesPage.jsx
@@ -18,7 +18,7 @@ const VersesPage = () => {
   });
   return (
     <div>
-      <div className="w-full relative bg-gray-100 overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[202px] box-border gap-[79px] tracking-[normal] mq450:gap-[20px_79px] mq750:gap-[39px_79px]">
+      <div className="w-full relative bg-gray-100 overflow-hidden flex flex-col items-start justify-start pt-0 px-0 pb-[202px] box-border gap-[50px] tracking-[normal] mq450:gap-[20px_79px] mq750:gap-[39px_79px]">
         <img
           className="w-full h-[100vh] absolute !m-[0] top-[0px] right-[0px] bottom-[0px] left-[0px] max-w-full overflow-hidden max-h-full object-cover"
           alt=""
