b1e21169aed1e3463b95ffa6878a102410fffef9
Yasser4Coder <by.benseghier.yasser@gmail.com>
2024-03-30 00:05:57 +0100

    ScoreBoard

--- src/components/MissionExplanationTech.jsx
+++ src/components/MissionExplanationTech.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const MissionExplanationTech = () => {
   return (
     <div className="conatiner relative z-[9] w-[1350px] m-auto flex flex-col items-center text-center ">
-      <div className="titel bg-red-600 mq975:text-[24px] Bord m-0 Bord text-inherit tracking-[0.12em] font-inherit text-transparent !bg-clip-text [background:linear-gradient(83.88deg,_#7d26cd,_#e2e2e2)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+      <div className="titel mq975:text-[24px] Bord m-0 Bord text-inherit tracking-[0.12em] font-inherit text-transparent !bg-clip-text [background:linear-gradient(83.88deg,_#7d26cd,_#e2e2e2)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
         <h1 className="text-[65px] lg:text-[55px] mq975:text-[44px]">
           Intro To The mISSIon
         </h1>
@@ -10,18 +10,26 @@ const MissionExplanationTech = () => {
       <div className="per relative top-[-50px] Segoe text-[30px] lg:text-[20px] mq975:text-[17px] text-white">
         <p>Each character explains their part in saving the world.</p>
       </div>
-      <div className="verses flex flex-col gap-[50px]">
-        <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] border-[3px] border-solid border-darkblue rounded-3xl">
-          <h1>One Piece: Artificial intelligence </h1>
+      <div className="verses flex flex-col gap-[50px] ">
+        <div className="px-[3px] py-[3px] rounded-6xl bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2]">
+          <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] rounded-3xl">
+            <h1>One Piece: Artificial intelligence</h1>
+          </div>
         </div>
-        <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] border-[3px] border-solid border-darkblue rounded-3xl">
-          <h1>SheRlock HOlmEs: Problem Solving</h1>
+        <div className="px-[3px] py-[3px] rounded-6xl bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2]">
+          <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] rounded-3xl">
+            <h1>SheRlock HOlmEs: Problem Solving</h1>
+          </div>
         </div>
-        <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] border-[3px] border-solid border-darkblue rounded-3xl">
-          <h1>OWAtch DOGs: Cyber Security </h1>
+        <div className="px-[3px] py-[3px] rounded-6xl bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2]">
+          <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] rounded-3xl">
+            <h1>WAtch DOGs: Cyber Security</h1>
+          </div>
         </div>
-        <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] border-[3px] border-solid border-darkblue rounded-3xl">
-          <h1>Dr STRanGe: Design</h1>
+        <div className="px-[3px] py-[3px] rounded-6xl bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2]">
+          <div className="vers cursor-pointer text-[20px] lg:text-[10px] mq975:text-[8px] text-white Bord px-[20px] py-[20px] [background:linear-gradient(90deg,_#7d26cd,_#171717)] rounded-3xl">
+            <h1>Dr STRanGe: Design</h1>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/ScoreBoard.jsx
+++ src/pages/ScoreBoard.jsx
@@ -13,50 +13,52 @@ const ScoreBoard = () => {
         className="bg w-full bg-no-repeat bg-cover  h-full relative  min-h-[100vh] max-w-full bg-gray-100 overflow-auto flex flex-col items-start justify-start pt-0 px-0 pb-[93px] box-border gap-[64px] tracking-[normal] mq450:gap-[16px_64px] mq750:gap-[32px_64px]"
       >
         <Header image={<LogoutButton />} event={<Logout />} />
-        <div className="w-[1200px] bg-gradient-to-r from-[#7D26CD]  px-[5px] py-[5px] to-[#E2E2E2]  rounded-6xl z-[5] max-w-full flex mx-auto">
-          <section className="max-w-full  w-full flex flex-col  justify-starth-full pb-[90px] shrink rounded-9xl pl-[30px] pr-[30px] ok bg-gray-100  items-center text-center text-45xl text-blueviolet-200  border-solid box-border lg:box-border mq750:gap-[28px_57px] mq750:pt-[23px] mq750:pb-[73px] mq750:box-border">
-            <h1 className="Bord text-[60px] tracking-[0.12em] font-normal text-transparent !bg-clip-text [background:linear-gradient(83.88deg,_#7d26cd,_#e2e2e2)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] inline-block shrink-0 max-w-full mq450:text-19xl">
-              Score Board
-            </h1>
-            <div className="table Segoe-semi-bold  flex-col">
-              <div className="table-row">
-                <h1>Team Name</h1>
-                <div className="second text-white">
-                  <h1>Badges</h1>
-                  <h1>Score</h1>
+        <div className="w-full max-w-[1200px] shrink mx-auto flex justify-center">
+          <section className="w-[1200px] z-[5] bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2] px-[4px] py-[4px] bg-blue-600 mx-[20px] rounded-9xl border-solid box-border">
+            <div className="w-full rounded-6xl flex flex-col justify-starth-full pb-[90px] shrink px-[30px] ok bg-gray-100 items-center text-center text-45xl text-blueviolet-200 box-border mq750:gap-[28px_57px] mq750:pt-[23px] mq750:pb-[73px] mq750:box-border">
+              <h1 className="Bord text-[60px] tracking-[0.12em] font-normal text-transparent !bg-clip-text [background:linear-gradient(83.88deg,_#7d26cd,_#e2e2e2)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent] inline-block shrink-0 max-w-full mq450:text-19xl">
+                Score Board
+              </h1>
+              <div className="table Segoe-semi-bold  flex-col">
+                <div className="table-row">
+                  <h1>Team Name</h1>
+                  <div className="second text-white">
+                    <h1>Badges</h1>
+                    <h1>Score</h1>
+                  </div>
                 </div>
-              </div>
-              <div className="table-row">
-                <h1>
-                  <span className="text-white">1</span>
-                  <span className="text-white">.</span>
-                  &nbsp;&nbsp; elec_team
-                </h1>
-                <div className="second text-white">
-                  <h1>4</h1>
-                  <h1>10000</h1>
+                <div className="table-row">
+                  <h1>
+                    <span className="text-white">1</span>
+                    <span className="text-white">.</span>
+                    &nbsp;&nbsp; elec_team
+                  </h1>
+                  <div className="second text-white">
+                    <h1>4</h1>
+                    <h1>10000</h1>
+                  </div>
                 </div>
-              </div>
-              <div className="table-row">
-                <h1>
-                  <span className="text-white">1</span>
-                  <span className="text-white">.</span>
-                  &nbsp;&nbsp; elec_team
-                </h1>
-                <div className="second text-white">
-                  <h1>4</h1>
-                  <h1>10000</h1>
+                <div className="table-row">
+                  <h1>
+                    <span className="text-white">1</span>
+                    <span className="text-white">.</span>
+                    &nbsp;&nbsp; elec_team
+                  </h1>
+                  <div className="second text-white">
+                    <h1>4</h1>
+                    <h1>10000</h1>
+                  </div>
                 </div>
-              </div>
-              <div className="table-row">
-                <h1>
-                  <span className="text-white">1</span>
-                  <span className="text-white">.</span>
-                  &nbsp;&nbsp; elec_team
-                </h1>
-                <div className="second text-white">
-                  <h1>4</h1>
-                  <h1>10000</h1>
+                <div className="table-row">
+                  <h1>
+                    <span className="text-white">1</span>
+                    <span className="text-white">.</span>
+                    &nbsp;&nbsp; elec_team
+                  </h1>
+                  <div className="second text-white">
+                    <h1>4</h1>
+                    <h1>10000</h1>
+                  </div>
                 </div>
               </div>
             </div>
