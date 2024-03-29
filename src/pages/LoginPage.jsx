ce1511d582d9d45137920306c131646328fc075e
Yasser4Coder <by.benseghier.yasser@gmail.com>
2024-03-29 22:45:43 +0100

    Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
       className=" bg-no-repeat bg-cover   bg w-full h-full min-h-[100vh] relative bg-gray-100 flex flex-col items-start justify-start pt-0 px-0 pb-9 object-cover box-border gap-[70px] tracking-[normal] mq450:gap-[28px_112px] mq750:gap-[56px_112px]"
     >
       <Header image={<LoginButton />} event={<Login />} />
-      <main className="self-stretch flex flex-row items-start justify-center py-0 pb-[25px] px-5 box-border max-w-full">
+      <main className="self-stretch flex flex-row items-start justify-center py-0 pb-[25px]  px-5 box-border max-w-full">
         <LoginForm />
       </main>
     </div>
--- src/pages/WelcomPage.jsx
+++ src/pages/WelcomPage.jsx
@@ -16,9 +16,8 @@ const WelcomePage = () => {
       className="w-full bg-no-repeat bg-cover  h-full min-h-[100vh] relative bg-gray-100 flex object-cover flex-col items-start justify-start pt-0 px-0 pb-9 box-border gap-[70px] tracking-[normal] mq450:gap-[28px_112px] mq975:gap-[56px_112px]"
     >
       <Header image={<LoginButton />} event={<Login />} />
-      <main className="self-stretch flex flex-row items-start justify-center py-0 px-10 box-border max-w-full">
-       <div className="bg-gradient-to-r from-[#7D26CD]  to-[#E2E2E2] px-[5px] py-[5px] rounded-6xl">
-       <section className="w-[1120px]  flex flex-col items-end justify-start gap-[30px] max-w-full text-center text-[48px] text-white font-bord-demo">
+      <div className="bg-gradient-to-r from-[#7D26CD] to-[#E2E2E2] px-[5px] py-[5px] rounded-6xl self-stretch flex flex-row items-start justify-center py-0 px-10 box-border w-[1200px] lg:w-[990px] mq1025:w-[750px] mx-auto max-w-full">
+        <section className="w-full flex flex-col items-end justify-start gap-[30px] max-w-full text-center text-[48px] text-white font-bord-demo">
           <div className="self-stretch rounded-9xl bg-gray-100 box-border overflow-hidden flex flex-col items-center justify-start pt-4 px-5 pb-[61.5px] gap-[89.5px] max-w-full z-[1]  mq700:gap-[22px_89.5px] mq700:pt-5 mq700:pb-10 mq700:box-border mq975:gap-[45px_89.5px]">
             <div className="w-[930px] h-2.5 flex flex-row items-start justify-center py-0 pr-2 pl-0 box-border max-w-full">
               <img
@@ -40,22 +39,19 @@ const WelcomePage = () => {
                 cyber security, and design.
               </h1>
             </div>
-          </div>
-          <div className="w-[1104px] flex flex-row items-start justify-center py-0 px-5 box-border max-w-full">
-            <button
-              onClick={handelClick}
-              className="cursor-pointer py-3.5 px-5 bg-[transparent] w-[378px] rounded-6xl [background:linear-gradient(92.09deg,_#7d26cd,_#171717)] box-border overflow-hidden shrink-0 flex flex-row items-start justify-center max-w-full z-[1] border-[1px] border-solid border-gainsboro-100 hover:bg-silver-200 hover:box-border hover:border-[1px] hover:border-solid hover:border-silver-100"
-            >
-              <div className="relative Bord text-21xl tracking-[0.16em] font-bord-demo text-white text-center cursor-pointer mq450:text-5xl mq975:text-13xl">
-                LoGin
-              </div>
-            </button>
-          </div>
+            <div className="w-[1104px] flex flex-row items-start justify-center py-0 px-5 box-border max-w-full">
+              <button
+                onClick={handelClick}
+                className="cursor-pointer py-3.5 px-5 bg-[transparent] w-[378px] rounded-6xl [background:linear-gradient(92.09deg,_#7d26cd,_#171717)] box-border overflow-hidden shrink-0 flex flex-row items-start justify-center max-w-full z-[1] border-[1px] border-solid border-gainsboro-100 hover:bg-silver-200 hover:box-border hover:border-[1px] hover:border-solid hover:border-silver-100"
+              >
+                <div className="relative Bord text-21xl tracking-[0.16em] font-bord-demo text-white text-center cursor-pointer mq450:text-5xl mq975:text-13xl">
+                  LoGin
+                </div>
+              </button>
             </div>
           </div>
         </section>
-       </div>
-      </main>
+      </div>
     </div>
   );
 };
